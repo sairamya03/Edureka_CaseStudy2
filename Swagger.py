cb73d4bc665764d3b53844db9264072867a66767
Sai Ramya Machavarapu <45190306+sairamya03@users.noreply.github.com>
2019-12-16 13:37:33 +0530

    Update Swagger.py

--- Swagger.py
+++ Swagger.py
@@ -10,5 +10,5 @@ Created on Mon Dec 16 12:05:28 2019
 import requests
 
 link = "https://petstore.swagger.io/v2/swagger.json"
-f = requests.get(link)
-print(f.text)
+f1 = requests.get(link)
+print(f1.text)
